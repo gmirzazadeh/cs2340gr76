1e36078f6020b548aaac0b4f581b6a986face6ac
Kayla Verdeflor <113073349+km-verde@users.noreply.github.com>
2024-02-03 03:55:40 -0500

    Update DashboardFragment.java
    Updated Dashboard

--- app/src/main/java/com/example/cs2340gr76/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/cs2340gr76/ui/dashboard/DashboardFragment.java
@@ -9,8 +9,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.cs2340gr76.R;
 import com.example.cs2340gr76.databinding.FragmentDashboardBinding;
+
 
 public class DashboardFragment extends Fragment {
 
@@ -26,7 +29,44 @@ public class DashboardFragment extends Fragment {
 
         final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.classesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment.findNavController(DashboardFragment.this)
+                        .navigate(R.id.action_navigation_dashboard_to_navigation_classes);
+            }
+        });
+
+        binding.assignmentsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment.findNavController(DashboardFragment.this)
+                        .navigate(R.id.action_navigation_dashboard_to_navigation_assignments);
+            }
+        });
+
+        binding.examsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment.findNavController(DashboardFragment.this)
+                        .navigate(R.id.action_navigation_dashboard_to_navigation_exams);
+            }
+        });
+
+        binding.todoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment.findNavController(DashboardFragment.this)
+                        .navigate(R.id.action_navigation_dashboard_to_navigation_todo);
+            }
+        });
     }
 
     @Override
