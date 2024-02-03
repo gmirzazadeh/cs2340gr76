6e7a77a5fcc678513faecec73189080b640e69cd
Kayla Verdeflor <113073349+km-verde@users.noreply.github.com>
2024-02-03 17:16:39 -0500

    Update ToDoFragment.java

--- app/src/main/java/com/example/cs2340gr76/ui/todo/ToDoFragment.java
+++ app/src/main/java/com/example/cs2340gr76/ui/todo/ToDoFragment.java
@@ -9,13 +9,23 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cs2340gr76.databinding.FragmentTodoBinding;
+import com.example.cs2340gr76.ui.todo.Adapter.ToDoAdapter;
+import com.example.cs2340gr76.ui.todo.Model.ToDoModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ToDoFragment extends Fragment {
 
     private FragmentTodoBinding binding;
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+    private List<ToDoModel> taskList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,6 +37,26 @@ public class ToDoFragment extends Fragment {
 
         final TextView textView = binding.textTodo;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        taskList = new ArrayList<>();
+
+        tasksRecyclerView = binding.tasksRecylerView;
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        tasksAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        ToDoModel task = new ToDoModel();
+        task.setTask("This is a test");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+        tasksAdapter.setTasks(taskList);
 
         return root;
     }
