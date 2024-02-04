bf54aba69ba5a7ab2deae22032768be11e91aca6
Kayla Verdeflor <113073349+km-verde@users.noreply.github.com>
2024-02-04 10:47:07 -0500

    Add files via upload

--- app/src/main/java/com/example/cs2340gr76/ui/todo/Adapter/ToDoAdapter.java
+++ app/src/main/java/com/example/cs2340gr76/ui/todo/Adapter/ToDoAdapter.java
@@ -1,16 +1,21 @@
 package com.example.cs2340gr76.ui.todo.Adapter;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cs2340gr76.R;
+import com.example.cs2340gr76.ui.todo.AddNewTask;
 import com.example.cs2340gr76.ui.todo.Model.ToDoModel;
 import com.example.cs2340gr76.ui.todo.ToDoFragment;
+import com.example.cs2340gr76.ui.todo.Utils.DatabaseHandler;
 
 import java.util.List;
 
@@ -18,15 +23,25 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
 
     private List<ToDoModel> todoList;
     private ToDoFragment fragment;
+    private DatabaseHandler db;
 
-    public ToDoAdapter(ToDoFragment fragment) {
+    public ToDoAdapter(DatabaseHandler db, ToDoFragment fragment) {
+        this.db= db;
         this.fragment = fragment;
     }
 
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        ToDoModel item = todoList.get(position);
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        db.openDatabase();
+        final ToDoModel item = todoList.get(position);
         holder.task.setText(item.getTask());
         holder.task.setChecked(toBoolean(item.getStatus()));
+        holder.task.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                db.updateStatus(item.getId(), 1);
+            } else {
+                db.updateStatus(item.getId(), 0);
+            }
+        });
     }
 
     public int getItemCount() {
@@ -39,13 +54,37 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
 
     public void setTasks(List<ToDoModel> todoList) {
         this.todoList = todoList;
-        notifyDataSetChanged();
+        notifyItemChanged(0);
     }
 
+    @NonNull
     public ViewHolder onCreateViewHolder(ViewGroup parent, int view) {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.todo_layout, parent, false);
+        db.getAllTasks();
         return new ViewHolder(itemView);
+    }
+
+    public void deleteItem(int position) {
+        ToDoModel item = todoList.get(position);
+        db.deleteTask(item.getId());
+        todoList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public Context getContext() {
+        return this.fragment.getContext();
+    }
+
+    public void editItem(int position) {
+        ToDoModel item = todoList.get(position);
+        Bundle bundle = new Bundle();
+        bundle.putInt("id", item.getId());
+        bundle.putString("task", item.getTask());
+        AddNewTask fragment = new AddNewTask();
+        fragment.setArguments(bundle);
+        fragment.show(this.fragment.getParentFragmentManager(), AddNewTask.TAG);
+        notifyItemChanged(position);
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
---
+++ app/src/main/java/com/example/cs2340gr76/ui/todo/RecylcerItemTouchHelper.java
@@ -0,0 +1,93 @@
+package com.example.cs2340gr76.ui.todo;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cs2340gr76.R;
+import com.example.cs2340gr76.ui.todo.Adapter.ToDoAdapter;
+
+public class RecylcerItemTouchHelper extends ItemTouchHelper.SimpleCallback {
+
+    private ToDoAdapter adapter;
+
+    public RecylcerItemTouchHelper(ToDoAdapter adapter) {
+        super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+        this.adapter = adapter;
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(final RecyclerView.ViewHolder viewHolder, int direction) {
+        final int position = viewHolder.getBindingAdapterPosition();
+        if (direction == ItemTouchHelper.LEFT) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(adapter.getContext());
+            builder.setTitle("Delete Task");
+            builder.setMessage("Are you sure you want to delete this task?");
+            builder.setPositiveButton("Confirm", (dialog, which) -> adapter.deleteItem(position));
+            builder.setNegativeButton("Cancel", (dialog, which) -> adapter.notifyItemChanged(viewHolder.getBindingAdapterPosition()));
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+        } else {
+            adapter.editItem(position);
+        }
+    }
+
+    @Override
+    public void onChildDraw(Canvas c,RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+        Drawable icon;
+        ColorDrawable background;
+
+        View itemView = viewHolder.itemView;
+        int backgroundCornerOffSet = 20;
+
+        if (dX > 0) {
+            icon = ContextCompat.getDrawable(adapter.getContext(), R.drawable.edit);
+            background = new ColorDrawable(ContextCompat.getColor(adapter.getContext(), R.color.teal_700));
+        } else {
+            icon = ContextCompat.getDrawable(adapter.getContext(), R.drawable.delete);
+            background = new ColorDrawable(Color.RED);
+        }
+
+        assert icon != null;
+        int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconBottom = iconTop + icon.getIntrinsicHeight();
+
+        if (dX > 0) {
+            int iconLeft = itemView.getLeft() + iconMargin;
+            int iconRight = itemView.getLeft() + iconMargin + icon.getIntrinsicWidth();
+            icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+            background.setBounds(itemView.getLeft(), itemView.getTop(), itemView.getLeft() + ((int) dX) + backgroundCornerOffSet, itemView.getBottom());
+        } else if (dX < 0) {
+            int iconLeft = itemView.getRight() - iconMargin - icon.getIntrinsicWidth();
+            int iconRight = itemView.getRight() - iconMargin;
+            icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+            background.setBounds(itemView.getRight() + ((int) dX) - backgroundCornerOffSet, itemView.getTop(),
+                    itemView.getRight(), itemView.getBottom());
+        } else {
+            background.setBounds(0,0,0,0);
+        }
+        background.draw(c);
+        icon.draw(c);
+    }
+}
+
