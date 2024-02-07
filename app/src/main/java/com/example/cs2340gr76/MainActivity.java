aef8c67f3734c25d3c07a0fa6ee5aec1771bd59e
Kayla Verdeflor <km.verdeflor@gmail.com>
2024-02-07 07:25:51 -0500

    Revert "all weekly calendar"
    This reverts commit 82d8e5d8260d4f3a9772bbc516b1a7f7674dd338.

--- app/src/main/java/com/example/cs2340gr76/MainActivity.java
+++ app/src/main/java/com/example/cs2340gr76/MainActivity.java
@@ -1,80 +1,40 @@
 package com.example.cs2340gr76;
 
-import static com.example.cs2340gr76.CalendarUtils.daysInMonthArray;
-import static com.example.cs2340gr76.CalendarUtils.monthYearFromDate;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cs2340gr76.databinding.ActivityMainBinding;
-import com.google.android.material.navigation.NavigationView;
 
-import java.lang.reflect.Array;
-import java.time.LocalDate;
-import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener {
+public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private TextView monthYearText;
-    private RecyclerView calendarRecyclerView;
-    private LocalDate selectedDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        CalendarUtils.selectedDate = LocalDate.now();
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-        initWidgets();
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.nav_host_fragment_activity_main);
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationView navView = findViewById(R.id.nav_view);
         NavigationUI.setupWithNavController(binding.navView, navController);
-    }
-
-    private void setMonthView() {
-        monthYearText.setText(monthYearFromDate((CalendarUtils.selectedDate)));
-        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalendarUtils.selectedDate);
-        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
-        calendarRecyclerView.setLayoutManager(layoutManager);
-        calendarRecyclerView.setAdapter(calendarAdapter);
-    }
-    private void initWidgets() {
-        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
-        monthYearText = findViewById(R.id.monthandYearTV);
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -84,29 +44,6 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    public void previousMonthAction(View view) {
-        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusMonths(1);
-        setMonthView();
-    }
-    public void nextMonthAction(View view) {
-        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusMonths(1);
-        setMonthView();
-    }
-
-    public void onItemClick(int position, LocalDate date) {
-        if(date != null) {
-            CalendarUtils.selectedDate = date;
-            setMonthView();
-        }
-    }
-
-    public void weeklyAction (View view) {
-        startActivity(new Intent(this, WeekViewActivity.class));
-    }
-    public void goHome (View view) {
-        startActivity(new Intent(this, WeekViewActivity.class));
     }
 
 }
