82d8e5d8260d4f3a9772bbc516b1a7f7674dd338
gmirzazadeh <gmirzazadeh@gatech.edu>
2024-02-06 23:02:40 -0500

    all weekly calendar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.cs2340gr76"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
